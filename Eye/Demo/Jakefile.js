066104a41f213a87311356ab577f759e8cc7d67f
Takuo KIHIRA <t-kihira@broadtail.jp>
2013-04-29 19:35:10 +0900

    created: Jakefile for demo

---
+++ Eye/Demo/Jakefile.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+var path = require("path");
+var util = require("util");
+var exec = require("child_process").exec;
+
+// default task
+var command_exec = function(command) {
+	exec(command, function(e, stdout, stderr) {
+		stdout && console.log(stdout);
+		stderr && console.log(stderr);
+	});
+};
+
+desc("build demo");
+task("default", [], function() {
+	command_exec("jsx --executable web --output demo.js Demo.jsx");
+});
+
