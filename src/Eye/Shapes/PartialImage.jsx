a98696c076a655e3c9b52f35b342875aa6ada590
Hironori Bono <bouno@cameo.plala.or.jp>
2013-07-08 16:05:20 +0900

    Add anchor points and implement multiplication of matrices.

--- src/Eye/Shapes/PartialImage.jsx
+++ src/Eye/Shapes/PartialImage.jsx
@@ -7,6 +7,8 @@ mixin PartialImage {
 	var top: number;
 	var width: number;
 	var height: number;
+	var anchorX: number;
+	var anchorY: number;
 	var _img: HTMLImageElement;
 	
 	function setRect(left: number, top: number, width: number, height: number): void {
@@ -14,9 +16,20 @@ mixin PartialImage {
 		this.top = top;
 		this.width = width;
 		this.height = height;
+		this.anchorX = 0;
+		this.anchorY = 0;
+	}
+
+	function setRect(left: number, top: number, width: number, height: number, anchorX:number, anchorY: number): void {
+		this.left = left;
+		this.top = top;
+		this.width = width;
+		this.height = height;
+		this.anchorX = anchorX;
+		this.anchorY = anchorY;
 	}
 	
 	override function draw(ctx: CanvasRenderingContext2D): void {
-		ctx.drawImage(this._img, this.left, this.top, this.width, this.height, 0, 0, this.width, this.height);
+		ctx.drawImage(this._img, this.left, this.top, this.width, this.height, this.anchorX, this.anchorY, this.width, this.height);
 	}
 }
