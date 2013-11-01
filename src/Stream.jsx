9a547227cfa4d36d13963cc60367c89b003475b7
Motohiro Takayama <mootoh@gmail.com>
2013-11-01 16:37:50 +0900

    number -> int.

--- src/Stream.jsx
+++ src/Stream.jsx
@@ -8,19 +8,19 @@ interface Stream {
 	function endEyeRender(): void;
 
 	// Layer
-	function sendLayerInfo(id: number, width: number, height: number, alpha: number, compositeOperation: string, layoutMode: int, layoutScale: number): void;
-	function endLayer(id: number): void;
+	function sendLayerInfo(id: int, width: int, height: int, alpha: number, compositeOperation: string, layoutMode: int, layoutScale: number): void;
+	function endLayer(id: int): void;
 
 	// DisplayNode
 	function sendDisplayNode(node:DisplayNode): void;
-	function sendSetTransform(layerId: number, nodeId: number, lastUpdatedFrame: int, sx : number, r0: number, r1: number, sy: number, tx: number, ty: number): void;
+	function sendSetTransform(layerId: int, nodeId: int, lastUpdatedFrame: int, sx : number, r0: number, r1: number, sy: number, tx: number, ty: number): void;
 
-	function sendSave(layerId: number): void;
-	function sendMatrix(layerId: number, sx : number, r0: number, r1: number, sy: number, tx: number, ty: number): void;
-	function sendCompositeOperation(layerId: number, operation: string): void;
-	function sendAlpha(layerId: number, alpha: number): void;
-	function sendRestore(layerId: number): void;
+	function sendSave(layerId: int): void;
+	function sendMatrix(layerId: int, sx : number, r0: number, r1: number, sy: number, tx: number, ty: number): void;
+	function sendCompositeOperation(layerId: int, operation: string): void;
+	function sendAlpha(layerId: int, alpha: number): void;
+	function sendRestore(layerId: int): void;
 
 	// Shape
-	function sendShape(layerId: number, nodeId: number, shape: Shape): void;
+	function sendShape(layerId: int, nodeId: int, shape: Shape): void;
 }
